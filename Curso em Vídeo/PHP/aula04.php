c959ca16fab5f4f037a1bf42c29bcc5c0c16d90a
Diogo Sobezak <96633512+diogobonet@users.noreply.github.com>
2022-09-26 21:18:07 -0300

    +PHP +HTML
    Today was fine!!

---
+++ Curso em Vídeo/PHP/aula04.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <title>Aula 04 -  PHP</title>
+</head>
+<body>
+    <div>
+        <?php
+            // $num = 4;
+            // $nome = "Diogo";
+            // $num = 4.5;
+            $nome = "Diogo";
+            $idade = 18;
+
+            echo "$nome tem 
+            $idade anos";
+        ?>
+    </div>
+</body>
+</html>
