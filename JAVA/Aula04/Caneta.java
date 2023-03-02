81ca87f4550555f7f6c98bfee1d2e19123faf029
Diogo Sobezak <96633512+diogobonet@users.noreply.github.com>
2023-03-02 16:19:12 -0300

    + JAVA POO

---
+++ JAVA/Aula04/Caneta.java
@@ -0,0 +1,49 @@
+package Aula04;
+
+public class Caneta {
+    public String modelo;
+    private float ponta;
+    private boolean tampada;
+    private String cor;
+
+
+    public Caneta(String m, float p, String c) { // Este é o método construtor
+        this.setModelo(m);
+        this.setPonta(p);
+        this.cor = c;
+        this.tampar();
+    }
+
+
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public void setModelo(String m) {
+        this.modelo = m;
+    }
+
+    public float getPonta() {
+        return this.ponta;
+    }
+
+    public void setPonta(float p) {
+        this.ponta = p;
+    }
+
+    public void tampar() {
+        this.tampada = true;
+    }
+
+    public void destampar() {
+        this.tampada = false;
+    }
+    public void status() {
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Ponta: " + this.getPonta());
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Tampada: " + this.tampada);
+    }
+}
+
