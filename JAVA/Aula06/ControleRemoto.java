c547f6040f739cb6a3e910a4b0ec2a8e3a16fd01
Diogo Sobezak <96633512+diogobonet@users.noreply.github.com>
2023-03-08 18:31:32 -0300

    +JAVA

---
+++ JAVA/Aula06/ControleRemoto.java
@@ -0,0 +1,112 @@
+package Aula06;
+
+public class ControleRemoto implements Controlador{
+    // Atributos
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+
+    // Metodos Especiais
+
+
+    public ControleRemoto() {
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando = false;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean getLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    public boolean getTocando() {
+        return tocando;
+    }
+
+    public void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+
+    // Metodos Abstratos
+
+    @Override
+    public void ligar() {
+        this.setLigado(true);
+    }
+
+    @Override
+    public void desligar() {
+        this.setLigado(false);
+    }
+
+    @Override
+    public void abrirMenu() {
+        System.out.println("Está ligado? " + this.getLigado());
+        System.out.println("Está tocando? " + this.getTocando());
+        System.out.println("Volume: " + this.getVolume());
+        for (int i = 0; i <= this.getVolume(); i += 10) {
+            System.out.print("[]");
+        }
+    }
+
+    @Override
+    public void fecharMenu() {
+        System.out.println("Fechando menu!");
+    }
+
+    @Override
+    public void maisVolume() {
+        if (getLigado()) {
+            this.setVolume(getVolume() + 5);
+        }
+    }
+
+    @Override
+    public void menosVolume() {
+        if (getLigado()) {
+            this.setVolume(getVolume() - 5);
+        }
+    }
+
+    @Override
+    public void ligarMudo() {
+        if (this.getLigado() && this.getVolume() > 0) {
+            this.setVolume(0);
+        }
+    }
+
+    @Override
+    public void desligaMudo() {
+        if (this.getLigado() && this.getVolume() == 0) {
+            this.setVolume(50);
+        }
+    }
+
+    @Override
+    public void play() {
+        if (this.getLigado() && !this.getTocando()) {
+            this.setTocando(true);
+        }
+    }
+
+    @Override
+    public void pause() {
+        if (this.getLigado() && this.getTocando()) {
+            this.setTocando(false);
+        }
+    }
+
+
+}
---
+++ JAVA/Aula06/main.java
@@ -0,0 +1,10 @@
+package Aula06;
+
+public class main {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.play();
+        c.abrirMenu();
+    }
+}
