f60bfd2a73f3c00f297690364933e5b2664aeae5
Diogo Sobezak <96633512+diogobonet@users.noreply.github.com>
2023-03-13 18:26:38 -0300

    + JAVA

---
+++ JAVA/Aula07/Lutador.java
@@ -0,0 +1,139 @@
+package Aula07;
+
+public class Lutador {
+    //Atributos
+    private String nome;
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String categoria;
+    private int vitorias, derrotas, empates;
+    //Métodos Públicos
+    public void apresentar() {
+        System.out.println("-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        System.out.println("Lutador: " + this.getNome());
+        System.out.println("Origem: " + this.getNacionalidade());
+        System.out.println("Idade: " + this.getIdade());
+        System.out.println("Altura: " + this.getAltura());
+        System.out.println("Pesando: " + this.getPeso());
+        System.out.println("Ganhou: " + this.getVitorias());
+        System.out.println("Derrotas: " + this.getDerrotas());
+        System.out.println("Empates " + this.getEmpates());
+    }
+
+    public void status() {
+        System.out.println(this.getNome() + "é um peso" + this.getCategoria());
+        System.out.println("Ganhou: " + this.getVitorias());
+        System.out.println("Derrotas: " + this.getDerrotas());
+        System.out.println("Empates " + this.getEmpates());
+    }
+
+    public void ganharLuta() {
+        this.setVitorias(this.getVitorias() + 1);
+    }
+
+    public void perderLuta() {
+        this.setDerrotas(this.getDerrotas() + 1);
+    }
+
+    public void empatarLuta() {
+        this.setEmpates(this.getEmpates() + 1);
+    }
+
+    // Metodo Construtor
+    public Lutador(String no, String na, int id, float al, float pe, int vi, int de, int em) {
+        this.nome = no;
+        this.nacionalidade = na;
+        this.idade = id;
+        this.altura = al;
+        this.peso = pe;
+        this.vitorias = vi;
+        this.derrotas = de;
+        this.empates = em;
+    }
+
+    // Getter and Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+        this.setCategoria();
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    private void setCategoria(String categoria) {
+        if (this.peso < 52.1) {
+            this.categoria = "Invalido";
+        } else if (peso <= 70.3) {
+            this.categoria = "Peso Leve!";
+        } else if (peso <= 83.9) {
+            this.categoria = "Peso médio!";
+        } else if (peso <= 120.2) {
+            this.categoria = "Peso pesado!";
+        } else {
+            this.categoria = "Inválido";
+        }
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
+}
