a9eaf0dd809e4b46322e4e9ef1b3471b93035118
Diogo Sobezak <96633512+diogobonet@users.noreply.github.com>
2023-03-14 10:24:04 -0300

    +JAVA

--- JAVA/Aula07/Aula07.java
+++
@@ -1,5 +0,0 @@
-package Aula07;
-
-public static void main(String[]args){
-    Lutador l1 = new Lutador();
-}
--- JAVA/Aula07/Lutador.java
+++ JAVA/Aula07/Lutador.java
@@ -92,7 +92,7 @@ public class Lutador {
 
     public void setPeso(float peso) {
         this.peso = peso;
-        this.setCategoria();
+        //this.setCategoria();
     }
 
     public String getCategoria() {
