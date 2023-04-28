bf8600a4d78c800c492466dc65647a090d97b91a
Diogo Sobezak <96633512+diogobonet@users.noreply.github.com>
2023-04-28 17:54:43 -0300

    +JS

---
+++ Cursos/CODERCURSOS/1 - Fundamentos/6 - Estrutura de Controle/switch.js
@@ -0,0 +1,28 @@
+const imprimirResultado = function (nota) {
+    switch(Math.ceil(nota)) {
+        case 10:
+        case 9:
+            console.log("Quadro de Honra!")
+            break
+        case 8: case 7:
+            console.log("Aprovado!")
+            break
+        case 6: case 5: case 4:
+            console.log("Recuperação!")
+            break
+        case 3: case 2: case 1:
+            console.log("Reprovado!")
+            break
+        default:
+            console.log("Nota inválida!")
+            break
+    }
+}
+
+
+console.log("Oiii")
+imprimirResultado(9)
+imprimirResultado(6.5)
+imprimirResultado(3)
+imprimirResultado(-1)
+console.log("Fim!")
