de2b6be874d8f440c4d496e9eb62b8e108f6bbfc
Diogo Sobezak <diogo.sobezak@pollysoft.com.br>
2023-07-10 17:27:01 -0300

    Parte 2 (C#)

--- Cursos/Balta.io/Fundamentos do C#/Teste/Program.cs
+++ Cursos/Balta.io/Fundamentos do C#/Teste/Program.cs
@@ -16,14 +16,17 @@ namespace Teste
             const int IDADE_MINIMA = 18;
             Console.Write(IDADE_MINIMA);
 
+            // ===============================================================
+
             /* TIPOS PRIMITIVOS DE DADOS */
             
             // BYTE (8-BIT) de 0-255
             // sbyte -128-127
 
             byte meuByte = 254;
-            sbyte meuByte = -127;
+            sbyte meuByte2 = -127;
 
+            // ===============================================================  
 
             /* NÚMEROS INTEIROS 
             
@@ -36,6 +39,45 @@ namespace Teste
             USHORT, UINT, ULONG (Somente números inteiros)
             */
 
+            // ===============================================================
+
+            /*
+            NÚMEROS REAIS
+            
+            FLOAT (32 bits) toda a vez que for float usar o "f" no final
+
+            DOUBLE (64 bits) não exige nada!
+
+            DECIMAL (128 bits) toda a vez que for decimal usar o "m" no final
+            
+            */
+
+            float men = 2.500f;
+
+            // ===============================================================
+
+            /*
+            
+            Boolean (8-bit)
+            Apenas true or false
+
+            */
+
+            bool usuarioCadastrado = true;
+            bool pagamentoRecebido = false;
+
+            // ===============================================================
+
+            /*     
+            CHAR (unicode) tem que ser definida com aspas simples
+            */
+
+            char primeiraLetra = 'D';
+
+
+            /*
+            String armazena uma cadeia de caracteres e tem que ser definida com aspas duplas
+            */
         }
     }
 }
