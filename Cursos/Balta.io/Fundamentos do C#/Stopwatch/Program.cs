a3777bae83bef393ba605f3e6ff42e5fc5c59da5
Diogo Sobezak <diogo.sobezak@pollysoftsoft.com.br>
2023-07-17 16:23:01 -0300

    Stopwatch finalizado

--- Cursos/Balta.io/Fundamentos do C#/Stopwatch/Program.cs
+++ Cursos/Balta.io/Fundamentos do C#/Stopwatch/Program.cs
@@ -6,7 +6,7 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Start(6);
+            Menu();
         }
 
         static void Start(int time)
@@ -21,15 +21,35 @@ namespace Stopwatch
             }
             Console.Clear();
             Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2500);
             Menu();
         }
 
         static void Menu()
         {
-            switch ()
-            {
+            Console.Clear();
+            Console.WriteLine("S = Segundos => 10s");
+            Console.WriteLine("M = Minutos => 10m");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
 
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+            {
+                multiplier = 60;
             }
+
+            if (time == 0) {
+                System.Environment.Exit(0);
+            }
+
+            Start(time * multiplier);
+
+
          }
     }
 }
