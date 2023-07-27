b9894a14679f747ed4975d21529cc7c78b935b84
Diogo Sobezak <diogo.sobezak@pollysoft.com.br>
2023-07-27 16:54:51 -0300

    Curso do balta

--- Cursos/Balta.io/Fundamentos do C#/Strings/Program.cs
+++ Cursos/Balta.io/Fundamentos do C#/Strings/Program.cs
@@ -82,6 +82,10 @@ namespace Strings
             Console.WriteLine(texto.Insert(5, "AQUI "));
             Console.WriteLine(texto.Remove(5, 5));
             Console.WriteLine(texto.Length);
+
+            /*
+             * Manipulação de Strings
+             */
         }
     }
 }
