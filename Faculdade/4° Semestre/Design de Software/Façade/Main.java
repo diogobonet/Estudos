2bc1aea66e68bfa47c40c14231c7af51d2e029fa
Diogo Sobezak <diogosobezak@gmail.com>
2023-09-28 17:07:59 -0300

    Façade - Arquitetura de Software

--- Faculdade/4° Semestre/Design de Software/Façade/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BankFacade bank = new BankFacade();
-        bank.addMoney(50);
-        bank.addMoney(500);
-        bank.consultValue();
-        bank.loanRequest(true, 5000);
-    }
-}
--- Faculdade/4° Semestre/Design de Software/Façade/User.java
+++ Faculdade/4° Semestre/Design de Software/Façade/User.java
@@ -1,2 +1,12 @@
-public class User {
+public class Main {
+    public static void main(String[] args) {
+        BankFacade bank = new BankFacade();
+        bank.addMoney(50);
+        bank.addMoney(500);
+        bank.consultValue();
+        bank.loanRequest(true, 5000);
+
+        User user1 = new User("Diogo", 0, 0);
+        user1.bankFacade.addMoney(50);
+    }
 }
